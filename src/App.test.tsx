fe16ea9d9e455cbaf41896759c6891c00c15c6f3
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2023-07-10 11:08:11 +0200

    Initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import { describe, it, expect } from "vitest";
+import { render } from "@testing-library/react";
+import * as matchers from "@testing-library/jest-dom/matchers";
+import App from "./App.tsx";
+
+expect.extend(matchers);
+
+describe("App", () => {
+    it("renders component", () => {
+        const { getByTestId } = render(<App />);
+        expect(getByTestId("app")).toBeInTheDocument();
+    });
+});
