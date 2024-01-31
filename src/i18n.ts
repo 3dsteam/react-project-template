d1e0a1c862497614f7fb77a9ae1a4fbea88bc32d
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2024-01-31 22:43:54 +0100

    fix: change import of i18next init function

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,16 +2,16 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import I18NextHttpBackend from "i18next-http-backend";
 
-void i18next
-    .use(I18NextHttpBackend)
-    .use(initReactI18next)
-    .init({
-        lng: localStorage.getItem("i18NextLng") || navigator.language,
-        fallbackLng: "en",
-        debug: process.env.NODE_ENV === "development",
-        interpolation: {
-            escapeValue: false,
-        },
-    });
-
-export default i18next;
+export default async function (fallbackLng?: string) {
+    await i18next
+        .use(I18NextHttpBackend)
+        .use(initReactI18next)
+        .init({
+            lng: localStorage.getItem("i18NextLng") ?? navigator.language?.split("-")[0]?.toLowerCase(),
+            fallbackLng: fallbackLng ?? "en",
+            debug: import.meta.env.DEV,
+            interpolation: {
+                escapeValue: false,
+            },
+        });
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "@store/index";
 import App from "./App.tsx";
-import "./i18n.ts";
+import i18n from "./i18n.ts";
 import "./global.scss";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <ReduxProvider store={store}>
-            <App />
-        </ReduxProvider>
-    </React.StrictMode>,
-);
+void (async () => {
+    // Initialize i18n
+    await i18n();
+    // Render App
+    ReactDOM.createRoot(document.getElementById("root")!).render(
+        <React.StrictMode>
+            <ReduxProvider store={store}>
+                <App />
+            </ReduxProvider>
+        </React.StrictMode>,
+    );
+})();
