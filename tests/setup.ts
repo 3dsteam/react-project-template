d6ed9b165553c68cdea437cc7e746605e31f51ee
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2024-02-02 17:31:18 +0100

    build: add and configure vitest-fetch-mock

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom/vitest";
+import "vitest-fetch-mock";
+
+import { vi } from "vitest";
+import createFetchMock from "vitest-fetch-mock";
+
+// Enable fetch mocking
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
