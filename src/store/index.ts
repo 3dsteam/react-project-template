9a7fb3c54e3dcf91d38376199e5081d247ed7f55
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2024-02-02 12:54:59 +0100

    feat: add RTK Query signInApi

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,17 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import auth from "@store/reducers/auth.ts";
+import { signInApi } from "@store/services/queries/sign-in.ts";
 
 export const rootReducer = combineReducers({
     auth,
+    // Services
+    signInApi: signInApi.reducer,
 });
 
 const store = configureStore({
     reducer: rootReducer,
     devTools: import.meta.env.DEV,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(signInApi.middleware),
 });
 
 export default store;
