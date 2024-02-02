f2bf921531c2b13499fd7237d769b72893818cc2
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2024-02-02 16:09:40 +0100

    feat: add sign-in page functionality for authentication

--- src/pages/sign-in/__tests__/sign-in.test.tsx
+++ src/pages/sign-in/__tests__/sign-in.test.tsx
@@ -1,19 +1,108 @@
-it.todo("renders the sign-in page");
+import { renderWithProviders } from "@store/test-utils.tsx";
+import { act, screen } from "@testing-library/react";
+import SignIn from "../sign-in";
+import SignInForm from "@components/sign-in-form";
+import * as signInApi from "@store/services/queries/sign-in.ts";
+import * as authStore from "@store/reducers/auth.ts";
+
+vi.mock("@components/sign-in-form");
+vi.mock("@store/services/queries/sign-in.ts", async () => ({
+    ...(await vi.importActual("@store/services/queries/sign-in.ts")),
+}));
+
+const signInMutation = vi.fn();
+
+beforeAll(() => {
+    // Spy on the authenticate action
+    vi.spyOn(authStore, "authenticate");
+    // Mocks the sign-in mutation
+    vi.spyOn(signInApi, "useSignInMutation").mockReturnValue([signInMutation, { originalArgs: {}, reset: vi.fn() }]);
+});
+
+beforeEach(() => {
+    renderWithProviders(<SignIn />);
+});
+
+it("renders the sign-in page", () => {
+    expect(screen.getByTestId("sign-in")).toBeInTheDocument();
+});
 
 describe("Elements render", () => {
-    it.todo("renders the logo");
+    it("renders the logo", () => {
+        expect(screen.getByAltText("Logo")).toBeInTheDocument();
+    });
 
-    it.todo("renders the sign-in form");
+    it("renders the sign-in form", () => {
+        expect(vi.mocked(SignInForm)).toHaveBeenCalled();
+    });
 });
 
 describe("When the sign-in form is submitted", () => {
-    it.todo("sends a request to the server");
+    beforeEach(async () => {
+        // onSubmit callback
+        await act(() =>
+            vi.mocked(SignInForm).mock.calls[0][0].onSubmit({
+                email: "lorem.ipsum@gmail.com",
+                password: "Ipsum1@34!",
+            }),
+        );
+    });
+
+    it("calls the sign-in mutation", () => {
+        expect(signInMutation).toHaveBeenCalledWith({
+            email: "lorem.ipsum@gmail.com",
+            password: "Ipsum1@34!",
+        });
+    });
 
     describe("When the request is successful", () => {
-        it.todo("calls the Redux authentication action");
+        beforeAll(() => {
+            // Mock the sign-in mutation response
+            vi.spyOn(signInApi, "useSignInMutation").mockReturnValue([
+                signInMutation,
+                {
+                    data: { token: "JWT Token", user: { id: "1", email: "lorem.ipsum@gmail.com" } },
+                    originalArgs: {},
+                    reset: vi.fn(),
+                },
+            ]);
+        });
+
+        it("calls the Redux authentication action", () => {
+            expect(vi.mocked(authStore.authenticate)).toHaveBeenCalledWith({
+                token: "JWT Token",
+                user: { id: "1", email: "lorem.ipsum@gmail.com" },
+            });
+        });
     });
 
     describe("When the request is unsuccessful", () => {
-        it.todo("displays an error message");
+        beforeAll(() => {
+            vi.clearAllMocks();
+            // Mock the sign-in mutation response
+            vi.spyOn(signInApi, "useSignInMutation").mockReturnValue([
+                signInMutation,
+                {
+                    error: {
+                        data: {
+                            requestId: "REQ#01",
+                            code: "UNAUTHORIZED",
+                            detail: "Username doesn't exist",
+                            message: "Username or password are incorrect",
+                        },
+                    },
+                    originalArgs: {},
+                    reset: vi.fn(),
+                },
+            ]);
+        });
+
+        it("doesn't call the Redux authentication action", () => {
+            expect(vi.mocked(authStore.authenticate)).not.toHaveBeenCalled();
+        });
+
+        it("displays an error message", () => {
+            expect(screen.getByTestId("error-message")).toHaveTextContent("Username or password are incorrect");
+        });
     });
 });
--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -1,3 +1,35 @@
+import Logo from "@assets/logo.svg";
+import SignInForm from "@components/sign-in-form";
+import { useSignInMutation } from "@store/services/queries/sign-in.ts";
+import { useEffect } from "react";
+import { useAppDispatch } from "@store/hooks.ts";
+import { authenticate } from "@store/reducers/auth.ts";
+import { IAPIError } from "@models/error.ts";
+
 export default function SignIn() {
-    return <div data-testid="sign-in">Sign In</div>;
+    const dispatch = useAppDispatch();
+    const [signInMutation, { data, error }] = useSignInMutation();
+
+    useEffect(() => {
+        if (!data) return;
+        // @TODO: Parse token and user from data
+        const token = (data as { token: string }).token;
+        const user = (data as { user: Record<string, string> }).user;
+        // Authenticate user
+        dispatch(authenticate({ token, user }));
+    }, [data, dispatch]);
+
+    const handleOnSubmit = async (data: Record<string, string>) => {
+        await signInMutation(data);
+    };
+
+    return (
+        <div data-testid="sign-in">
+            <img src={Logo} alt="Logo" />
+            {/* Form */}
+            <SignInForm onSubmit={handleOnSubmit} />
+            {/* Error message */}
+            {error && "data" in error && <div data-testid="error-message">{(error.data as IAPIError).message}</div>}
+        </div>
+    );
 }
