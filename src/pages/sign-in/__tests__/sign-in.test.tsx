05e8df924ef08d8af380f7f9c2075979548ae70a
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2024-02-02 11:32:13 +0100

    feat: setup sign-in page with the test case

---
+++ src/pages/sign-in/__tests__/sign-in.test.tsx
@@ -0,0 +1,19 @@
+it.todo("renders the sign-in page");
+
+describe("Elements render", () => {
+    it.todo("renders the logo");
+
+    it.todo("renders the sign-in form");
+});
+
+describe("When the sign-in form is submitted", () => {
+    it.todo("sends a request to the server");
+
+    describe("When the request is successful", () => {
+        it.todo("calls the Redux authentication action");
+    });
+
+    describe("When the request is unsuccessful", () => {
+        it.todo("displays an error message");
+    });
+});
---
+++ src/pages/sign-in/sign-in.tsx
@@ -0,0 +1,3 @@
+export default function SignIn() {
+    return <div data-testid="sign-in">Sign In</div>;
+}
