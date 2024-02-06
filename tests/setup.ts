769ef0fb4e2eb147adb4354daa4ec45214582530
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2024-02-06 12:32:44 +0100

    test: add react-i18next mock for the tests

--- tests/setup.ts
+++ tests/setup.ts
@@ -7,3 +7,16 @@ import createFetchMock from "vitest-fetch-mock";
 // Enable fetch mocking
 const fetchMocker = createFetchMock(vi);
 fetchMocker.enableMocks();
+
+// i18next
+vi.mock("react-i18next", () => ({
+    // this mock makes sure any components using the translation hook can use it without a warning being shown
+    useTranslation: () => {
+        return {
+            t: (str: string) => str,
+            i18n: {
+                changeLanguage: () => Promise.resolve(),
+            },
+        };
+    },
+}));
