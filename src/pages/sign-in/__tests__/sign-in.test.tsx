0f8e7cf950216b10382872e7933231a00560fe0f
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2024-02-27 09:58:46 +0100

    tests: refactor API mock with MSW

--- src/pages/sign-in/__tests__/sign-in.test.tsx
+++ src/pages/sign-in/__tests__/sign-in.test.tsx
@@ -3,7 +3,8 @@ import { act, screen, waitFor } from "@testing-library/react";
 import SignIn from "../sign-in";
 import SignInForm from "@components/sign-in-form";
 import * as authStore from "@store/reducers/auth.ts";
-import { afterEach } from "vitest";
+import { server } from "@tests/mocks.ts";
+import { http, HttpResponse } from "msw";
 
 vi.mock("@components/sign-in-form");
 
@@ -45,18 +46,45 @@ describe("When the sign-in form is submitted", () => {
         );
     });
 
-    describe.skip("When the request is successful", () => {
+    describe("When the request is successful", () => {
         it("calls the Redux authentication action", async () => {
             await waitFor(() =>
                 expect(vi.mocked(authStore.authenticate)).toHaveBeenCalledWith({
-                    token: "JWT Token",
-                    user: { id: "1", email: "lorem.ipsum@gmail.com" },
+                    token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2OTg0MjIyOTIsImV4cCI6MTcyOTk1ODI5MywiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSJ9.15kvzcmttOpTXRTX2X1UQbgpzn4IiES2q9ohj7WI9ac",
+                    user: {
+                        id: "df0fb575-8738-4ebe-bbdf-4f494f378c09",
+                        email: "lorem.ipsum@gmail.com",
+                        permissions: [],
+                    },
                 }),
             );
         });
     });
 
-    describe.skip("When the request is unsuccessful", () => {
+    describe("When the request is unsuccessful", () => {
+        beforeAll(() => {
+            server.use(
+                http.post("*/sign-in", () => {
+                    return HttpResponse.json(
+                        {
+                            error: {
+                                requestId: "REQ_018dc6e6460b7b9abb3097435a64e041",
+                                code: "UNAUTHORIZED",
+                                detail: "Username not found",
+                                message: "Username or password are incorrect",
+                            },
+                        },
+                        { status: 401 },
+                    );
+                }),
+            );
+        });
+
+        afterAll(() => {
+            // Reset the server handlers
+            server.resetHandlers();
+        });
+
         it("doesn't call the Redux authentication action", () => {
             expect(vi.mocked(authStore.authenticate)).not.toHaveBeenCalled();
         });
