9241be6c162f4ece47d4c53cbb007a29df2883ed
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2024-09-03 14:18:01 +0200

    feat: create sign-in form with dynamic form component

--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -1,10 +1,10 @@
 import Logo from "@assets/logo.svg";
-import SignInForm from "@components/sign-in-form";
 import { useSignInMutation } from "@store/services/queries/sign-in.ts";
 import { useEffect } from "react";
 import { useAppDispatch } from "@store/hooks.ts";
 import { authenticate } from "@store/reducers/auth.ts";
 import { IAPIError } from "@models/error.ts";
+import DynamicForm, { EFieldType } from "@3dsteam/react-dynamic-form";
 
 export default function SignIn() {
     const dispatch = useAppDispatch();
@@ -21,18 +21,38 @@ export default function SignIn() {
     };
 
     return (
-        <div data-testid="sign-in" className="flex justify-center items-center flex-row w-screen h-screen bg-primary">
-            <div className="bg-white p-8 rounded-lg shadow-lg space-y-8">
-                <img src={Logo} alt="Logo" className="object-contain w-24 mx-auto" />
+        <div data-testid="sign-in" className="flex justify-end h-screen bg-primary">
+            <div className="basis-1/3 bg-white py-24 px-16 space-y-8">
+                {/* Logo */}
+                <img src={Logo} alt="Logo" className="object-contain h-40" />
                 {/* Form */}
-                <section className="space-y-2">
-                    <h1 className="text-2xl">Sign in</h1>
-                    <SignInForm onSubmit={handleOnSubmit} />
-                    {/* Error message */}
-                    {error && "data" in error && (
-                        <div data-testid="error-message">{(error.data as IAPIError).message}</div>
-                    )}
-                </section>
+                <h1 className="text-2xl font-bold">Sign in</h1>
+                <DynamicForm
+                    fields={[
+                        {
+                            name: "identification",
+                            type: EFieldType.EMAIL,
+                            placeholder: "Identification",
+                            validations: { required: true, isEmail: true },
+                            props: { floatLabelType: "Always" },
+                        },
+                        {
+                            name: "password",
+                            type: EFieldType.PASSWORD,
+                            placeholder: "Password",
+                            validations: { required: true },
+                            props: { floatLabelType: "Always" },
+                        },
+                    ]}
+                    buttons={{
+                        btnSubmitText: "Sign in",
+                        btnSubmitProps: { className: "w-full" },
+                    }}
+                    onSubmit={(data) => handleOnSubmit(data as Record<string, string>)}
+                    className="space-y-2"
+                />
+                {/* Error message */}
+                {error && "data" in error && <div data-testid="error-message">{(error.data as IAPIError).message}</div>}
             </div>
         </div>
     );
