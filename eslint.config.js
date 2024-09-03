e543ee2a9059591f38c4b933b8e3054a7045156e
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2024-09-03 14:14:50 +0200

    chore: update eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,50 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import eslint from "@eslint/js";
 import tsEslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
-import eslintConfigPrettier from "eslint-config-prettier";
+import reactEslint from "eslint-plugin-react";
+import prettierEslint from "eslint-config-prettier";
+import globals from "globals";
 
-export default [
-    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-    { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tsEslint.configs.recommended,
-    ...fixupConfigRules(pluginReactConfig),
-    eslintConfigPrettier
-];
+export default tsEslint.config(
+    { ignores: ["node_modules", "dist"] },
+    eslint.configs.recommended,
+    // TypeScript specific rules
+    ...tsEslint.configs.recommendedTypeChecked,
+    // ...tsEslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+                EXPERIMENTAL_useProjectService: true,
+            },
+        },
+    },
+    {
+        files: ["**/*.js"],
+        ...tsEslint.configs.disableTypeChecked,
+    },
+    // React specific rules
+    {
+        files: ["src/**/*.{ts,tsx}"],
+        ...reactEslint.configs.flat.recommended,
+        languageOptions: {
+            ...reactEslint.configs.flat.recommended.languageOptions,
+            globals: {
+                ...globals.browser,
+            },
+        },
+        rules: {
+            ...reactEslint.configs.flat.recommended.rules,
+            "react/react-in-jsx-scope": "off",
+            "react/forbid-component-props": [1, { forbid: ["style"] }],
+            "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+    },
+    // Prettier Overrides
+    prettierEslint,
+);
