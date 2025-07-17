1cd0edbbb2b342724193088e64e736558f378a0e
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2025-07-17 15:02:40 +0200

    chore: set default query client error type

--- src/integrations/tanstack-query/root-provider.tsx
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -1,7 +1,15 @@
+import type { IApiError } from "@models/error";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { PropsWithChildren } from "react";
 
 const queryClient = new QueryClient();
+
+// Define default error for the query client
+declare module "@tanstack/react-query" {
+    interface Register {
+        defaultError: IApiError;
+    }
+}
 
 export function getContext() {
     return {
